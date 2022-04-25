071b966bdc080d7683b2f205d89d353734e25766
aleqsunder <al.eq@outlook.com>
2022-04-26 00:45:39 +0400

    Дополнительная tampermonkey поддержка

--- src/js/_.js
+++ src/js/_.js
@@ -2,7 +2,9 @@
 // @name         WASD COMMENTS
 // @namespace    https://wasd.tv/
 // @match        https://wasd.tv/*
-// @version      0.1
+// @version      0.1.1
+// @updateURL    https://github.com/aleqsunder/wasd-record-comments/raw/master/dist/index.user.js
+// @downloadURL  https://github.com/aleqsunder/wasd-record-comments/raw/master/dist/index.user.js
 // @description  Получаем комментарии к записи WASD
 // @author       aleqsunder
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=tampermonkey.net
--- webpack.mix.js
+++ webpack.mix.js
@@ -9,7 +9,7 @@ mix.after(async stats => {
     const fs = require("fs").promises
     const tamperMonkeyData = await fs.readFile('./src/js/_.js', "utf-8")
     const compiledData = await fs.readFile(path, "utf-8")
-    const dataTemplate = `${tamperMonkeyData}\r\n${compiledData}`
+    const dataTemplate = `${tamperMonkeyData}\r\n\r\n${compiledData}`
     
     await fs.writeFile(path, dataTemplate, errors => {
         if (errors) {
