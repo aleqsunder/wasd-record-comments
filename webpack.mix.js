2530f0c9fe929d632de602386a70b076d73bf1b4
aleqsunder <al.eq@outlook.com>
2022-04-26 00:22:48 +0400

    DEVELOP-2 Автосборка + tampermonkey в minified файле

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,19 @@
+const path = "./dist/index.js"
 const mix = require('laravel-mix')
-mix.js('src/index.js', 'dist/index.js').vue()
+mix.js('src/index.js', path).vue()
 
 mix.disableNotifications()
 mix.disableSuccessNotifications()
+
+mix.after(async stats => {
+    const fs = require("fs").promises
+    const tamperMonkeyData = await fs.readFile('./src/js/_.js', "utf-8")
+    const compiledData = await fs.readFile(path, "utf-8")
+    const dataTemplate = `${tamperMonkeyData}\r\n${compiledData}`
+    
+    await fs.writeFile(path, dataTemplate, errors => {
+        if (errors) {
+            return console.log(errors)
+        }
+    })
+})
