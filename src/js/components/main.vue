c2db4102c5c5887d4803dbaab032467dcf44df7d
aleqsunder <al.eq@outlook.com>
2022-04-25 23:41:55 +0400

    DEVELOP-1 Переписал Vue на компоненты

---
+++ src/js/components/main.vue
@@ -0,0 +1,68 @@
+<template>
+  <template v-if="isLoading === false">
+    <div class="block-wrapper">
+      <TransitionGroup name="list">
+        <Message v-for="message in messages" :key="message.id" :message="message"/>
+      </TransitionGroup>
+    </div>
+  </template>
+  <div class="text-center" v-else v-text="loadingProgress"/>
+</template>
+
+<script>
+import Message from "./message"
+
+export default {
+  name: 'main',
+  components: {
+    Message
+  },
+
+  data() {
+    return {
+      colors: {},
+      messages: [],
+      isLoading: true,
+      loadingProgress: 0,
+    }
+  },
+
+  methods: {
+    randomHex() {
+      return Math.random() * 255 ^ 0
+    },
+
+    randomColor() {
+      return `color: rgb(${this.randomHex()}, ${this.randomHex()}, ${this.randomHex()})`
+    },
+
+    setMessages({detail: {messages}}) {
+      messages = messages.map(message => {
+        if (!this.colors.hasOwnProperty(message.info.user_id)) {
+          this.colors[message.info.user_id] = this.randomColor()
+        }
+        message.info.color = this.colors[message.info.user_id]
+        return message
+      })
+      this.messages = messages
+
+      setTimeout(() => {
+        const wrap = document.querySelector('.chat-container__wrap .block-wrapper')
+        wrap.scrollTop = wrap.scrollHeight
+      })
+    },
+
+    setLoading({detail}) {
+      this.isLoading = detail.isLoading
+      if (detail.loadingProgress > 0) {
+        this.loadingProgress = detail.loadingProgress
+      }
+    }
+  },
+
+  mounted() {
+    document.addEventListener('MessagesObserver:last50', this.setMessages)
+    document.addEventListener('MessagesObserver:loadingProgress', this.setLoading)
+  }
+}
+</script>
